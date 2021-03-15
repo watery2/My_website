11081794931d587d3dc51a0c171f219a8b2e4b43
watery2 <dominykasolrovas7@gmail.com>
2021-03-15 19:42:26 +0200

    couple changes

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, redirect, url_for, render_template, request, session
 app = Flask(__name__)
 app.secret_key = "fz47"
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        email = request.form["email"]
+        msg = request.form["Message"]
+        return redirect(request.url)
     return render_template("home.html")
 
 
